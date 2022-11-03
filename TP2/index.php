2ddcec1d7f45fc62d74cbee11c0ce6dd7372a927
BOTC3PO <javier.perez1314@gmail.com>
2022-11-03 10:53:33 -0300

    config creado

---
+++ TP2/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--no cache-->
+    <meta http-equiv="Expires" content="0">
+
+    <meta http-equiv="Last-Modified" content="0">
+
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+
+    <meta http-equiv="Pragma" content="no-cache">
+    <link rel="shortcut icon" href="src/favicon/favicon.jpg" type="image/x-icon">
+
+    <title>indice</title>
+    <link href="src/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"
+        type="text/css">
+    <link rel="stylesheet" href="src/css/style.css" type="text/css">
+
+</head>
